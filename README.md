ba74acf3c969f28414d2d016a025d567b75d75df
Brandon Horst <brandonhorst@gmail.com>
2015-02-12 11:37:39 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # simple-json-schema
-Simple JSON schema implementation designed to be used for JSON configuration files
+We live in a post-Sublime Text age. The cool way to store persistant state is in raw JSON. `simple-json-schema` makes it easy to make some simple assertions about what is in that JSON before passing it to your application logic.
+
+It works similar to the [Config system in the Atom Editor](https://atom.io/docs/api/v0.179.0/Config) but without coercion and with custom types.
+
+## Installation
+
+```sh
+npm install simple-json-schema
+```
+
+## Usage
+
+```js
+var parser = new (require('./.'))()
+var schema = {
+  type: 'object',
+  properties: {
+    name: {type: 'string'},
+    age: {type: 'integer', min: 0, max: 120},
+    income: {type: 'number', min: 0},
+    universe: {type: 'string', enum: ['Marvel', 'DC']},
+    living: {type: 'boolean', default: true},
+    alterEgos: {type: 'array', items: {type: 'string'}},
+    location: {
+      type: 'object',
+      properties: {
+        city: {type: 'string'},
+        state: {type: 'string', regex: /[A-Z]{2}/}
+      }
+    }
+  }
+}
+
+var object = {
+  name: 'Peter Parker',
+  age: 17,
+  income: 38123.52,
+  alterEgos: ['Spider-Man'],
+  universe: 'Marvel',
+  location: {city: 'New York', state: 'NY'},
+  girlfriend: 'Mary Jane'
+}
+
+console.log(parser.validateSchema(schema)) // true
+console.log(parser.validate(object, schema)) // true
+console.log(parser.normalize(object, schema)) /* {
+  name: 'Peter Parker',
+  age: 17,
+  income: 38123.52,
+  living: true,
+  alterEgos: ['Spider-Man'],
+  universe: 'Marvel',
+  location: {city: 'New York', state: 'NY'}
+} */
+```
+
+## Methods
+
+#### `parser.validateSchema(schema)`
+
+- Returns a boolean indicating if `schema` is valid.
+
+#### `parser.validate(obj, schema)`
+
+- Returns a boolean indicating if `obj` is valid according to `schema`
+
+#### `parser.normalize(obj, schema)`
+
+- Returns an adjusted representation of `obj`, filling in defaults and removing unnecessary fields.
+- Throws an error if `validateSchema` returns `false`
+
+## Custom Types
+
+You can add custom types to the parser by setting properties on `parser.types`. By default, it understands `integer`, `number`, `string`, `boolean`, `array`, and `object`. You can also `delete` those types if you need to.
+
+```js
+parser.types['typeName'] = {
+  validateSchema: function (schema, parser) {...},
+  validate: function (obj, schema, parser) {...},
+  normalize: function (obj, schema, parser) {...} // optional
+}
+```
+
+#### `validateSchema(schema, parser)`
+
+- Should return a boolean indicating if `schema` is valid.
+- `parser` is provided for subschema parsing (see `lib/array`).
+
+#### `validate(obj, schema, parser)`
+
+- should return a boolean indiciating if the `obj` fits `schema`.
+- `parser` is provided for subschema parsing (see `lib/array`).
+
+#### `normalize(obj, schema, parser) // optional`
+- should return a version of obj adjusted to fit the schema
+- if not provided, `normalize` will return `default` if it exists, or `undefined`
+- do not use this to coerce values - this should only be used for adjusting subschema parsing
+- see `lib/object` for an example
---
+++ test/test.js
@@ -0,0 +1,239 @@
+/*eslint-env mocha*/
+var chai = require('chai')
+var expect = chai.expect
+var SchemaManager = require('..')
+
+chai.config.includeStack = true
+
+describe('builtins', function () {
+  var s = new SchemaManager()
+  describe('general', function () {
+    it('getDefault', function () {
+      var schema = {type: 'integer', default: 4}
+      expect(s.getDefault(schema)).to.equal(4)
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'integer', enum: [1, 2], default: 2})).to.be.true
+      expect(s.validateSchema({type: 'integer', enum: [1, 2], max: 2})).to.be.true
+      expect(s.validateSchema({type: 'integer', default: 1, max: 2})).to.be.true
+
+      expect(s.validateSchema({type: 'integer', enum: [0, 1], default: 2})).to.be.false
+      expect(s.validateSchema({type: 'integer', enum: [0, 1], min: 2})).to.be.false
+      expect(s.validateSchema({type: 'integer', default: 1, min: 2})).to.be.false
+
+      expect(s.validateSchema({type: 'flobnarb'})).to.be.false
+    })
+    it('normalize', function () {
+      expect(s.normalize(undefined, {type: 'integer'})).to.be.undefined
+      expect(s.normalize(null, {type: 'integer'})).to.be.undefined
+      expect(s.normalize('x', {type: 'integer'})).to.be.undefined
+      expect(s.normalize(undefined, {type: 'integer', default: 3})).to.be.equal(3)
+      expect(s.normalize(null, {type: 'integer', default: 3})).to.be.equal(3)
+      expect(s.normalize('x', {type: 'integer', default: 3})).to.equal(3)
+
+      expect(s.normalize(3, {type: 'integer'})).to.equal(3)
+
+      expect(function () {
+        s.normalize({type: 'flobnarb'})
+      }).to.throw(Error)
+    })
+  })
+
+  describe('integer', function () {
+    it('validate type', function () {
+      var schema = {type: 'integer'}
+      expect(s.validateSchema(schema)).to.be.true
+      expect(s.validate(1, schema)).to.be.true
+      expect(s.validate('1', schema)).to.be.false
+      expect(s.validate('1ab', schema)).to.be.false
+      expect(s.validate('str', schema)).to.be.false
+      expect(s.validate({}, schema)).to.be.false
+      expect(s.validate(1.1, schema)).to.be.false
+      expect(s.validate(-1.2, schema)).to.be.false
+      expect(s.validate(true, schema)).to.be.false
+      expect(s.validate(false, schema)).to.be.false
+      expect(s.validate([], schema)).to.be.false
+      expect(s.validate([1], schema)).to.be.false
+    })
+    it('validate min', function () {
+      var schema = {type: 'integer', min: 2}
+      expect(s.validateSchema(schema)).to.be.true
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate(2, schema)).to.be.true
+      expect(s.validate(3, schema)).to.be.true
+    })
+    it('validate max', function () {
+      var schema = {type: 'integer', max: 0}
+      expect(s.validateSchema(schema)).to.be.true
+      expect(s.validate(-1, schema)).to.be.true
+      expect(s.validate(0, schema)).to.be.true
+      expect(s.validate(1, schema)).to.be.false
+    })
+    it('validate max + min', function () {
+      var schema = {type: 'integer', min: 1, max: 3}
+      expect(s.validateSchema(schema)).to.be.true
+      expect(s.validate(0, schema)).to.be.false
+      expect(s.validate(1, schema)).to.be.true
+      expect(s.validate(3, schema)).to.be.true
+      expect(s.validate(4, schema)).to.be.false
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'integer'})).to.be.true
+      expect(s.validateSchema({type: 'integer', min: 3})).to.be.true
+      expect(s.validateSchema({type: 'integer', max: 2})).to.be.true
+      expect(s.validateSchema({type: 'integer', max: 3, min: 2})).to.be.true
+      expect(s.validateSchema({type: 'integer', max: 2, min: 3})).to.be.false
+      expect(s.validateSchema({type: 'integer', default: '4'})).to.be.false
+      expect(s.validateSchema({type: 'integer', default: 1.4})).to.be.false
+      expect(s.validateSchema({type: 'integer', max: '4'})).to.be.false
+      expect(s.validateSchema({type: 'integer', max: 1.4})).to.be.false
+      expect(s.validateSchema({type: 'integer', min: '4'})).to.be.false
+      expect(s.validateSchema({type: 'integer', min: 1.4})).to.be.false
+    })
+  })
+
+  describe('number', function () {
+    it('validate type', function () {
+      var schema = {type: 'number'}
+      expect(s.validate(1, schema)).to.be.true
+      expect(s.validate(1.1, schema)).to.be.true
+      expect(s.validate(-1.2, schema)).to.be.true
+      expect(s.validate('1', schema)).to.be.false
+      expect(s.validate('1ab', schema)).to.be.false
+      expect(s.validate('str', schema)).to.be.false
+      expect(s.validate({}, schema)).to.be.false
+      expect(s.validate(true, schema)).to.be.false
+      expect(s.validate(false, schema)).to.be.false
+      expect(s.validate([], schema)).to.be.false
+      expect(s.validate([1], schema)).to.be.false
+    })
+    it('validate min', function () {
+      var schema = {type: 'number', min: 2.2}
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate(2, schema)).to.be.false
+      expect(s.validate(3, schema)).to.be.true
+    })
+    it('validate max', function () {
+      var schema = {type: 'number', max: 0.5}
+      expect(s.validate(-1, schema)).to.be.true
+      expect(s.validate(0, schema)).to.be.true
+      expect(s.validate(1, schema)).to.be.false
+    })
+    it('validate min + max', function () {
+      var schema = {type: 'number', min: 1.5, max: 3.5}
+      expect(s.validate(0, schema)).to.be.false
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate(2, schema)).to.be.true
+      expect(s.validate(3, schema)).to.be.true
+      expect(s.validate(4, schema)).to.be.false
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'number'})).to.be.true
+      expect(s.validateSchema({type: 'number', min: 2.5})).to.be.true
+      expect(s.validateSchema({type: 'number', max: 3.5})).to.be.true
+      expect(s.validateSchema({type: 'number', max: 3.5, min: 2.5})).to.be.true
+      expect(s.validateSchema({type: 'number', max: 2.5, min: 3.5})).to.be.false
+      expect(s.validateSchema({type: 'number', default: '4'})).to.be.false
+      expect(s.validateSchema({type: 'number', max: '4'})).to.be.false
+      expect(s.validateSchema({type: 'number', min: '4'})).to.be.false
+    })
+  })
+
+  describe('string', function () {
+    it('validate type', function () {
+      var schema = {type: 'string'}
+      expect(s.validate('1', schema)).to.be.true
+      expect(s.validate('1ab', schema)).to.be.true
+      expect(s.validate('str', schema)).to.be.true
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate({}, schema)).to.be.false
+      expect(s.validate(-1.2, schema)).to.be.false
+      expect(s.validate(true, schema)).to.be.false
+      expect(s.validate(false, schema)).to.be.false
+      expect(s.validate([], schema)).to.be.false
+      expect(s.validate(['str'], schema)).to.be.false
+    })
+    it('validate regex', function () {
+      var schemas = [
+        {type: 'string', regex: /a/},
+        {type: 'string', regex: 'a'}
+      ]
+      schemas.forEach(function (schema) {
+        expect(s.validate('a', schema)).to.be.true
+        expect(s.validate('abc', schema)).to.be.true
+        expect(s.validate('b', schema)).to.be.false
+      })
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'string'})).to.be.true
+      expect(s.validateSchema({type: 'string', regex: 'a'})).to.be.true
+      expect(s.validateSchema({type: 'string', regex: /a/})).to.be.true
+      expect(s.validateSchema({type: 'string', regex: 4})).to.be.false
+      expect(s.validateSchema({type: 'string', regex: '\\'})).to.be.false
+    })
+  })
+
+  describe('boolean', function () {
+    it('validate type', function () {
+      var schema = {type: 'boolean'}
+      expect(s.validate(true, schema)).to.be.true
+      expect(s.validate(false, schema)).to.be.true
+      expect(s.validate('str', schema)).to.be.false
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate({}, schema)).to.be.false
+      expect(s.validate([], schema)).to.be.false
+      expect(s.validate([true], schema)).to.be.false
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'boolean'})).to.be.true
+    })
+  })
+
+  describe('array', function () {
+    it('validate type', function () {
+      var schema = {type: 'array', items: {type: 'integer'}}
+      expect(s.validate([], schema)).to.be.true
+      expect(s.validate([1, 2, 3], schema)).to.be.true
+      expect(s.validate(['1'], schema)).to.be.false
+      expect(s.validate([1, '2'], schema)).to.be.false
+      expect(s.validate([1, [2, 3]], schema)).to.be.false
+      expect(s.validate('str', schema)).to.be.false
+      expect(s.validate(false, schema)).to.be.false
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate({}, schema)).to.be.false
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'array', items: {type: 'integer'}})).to.be.true
+      expect(s.validateSchema({type: 'array'})).to.be.false
+    })
+  })
+
+  describe('object', function () {
+    it('validate type', function () {
+      var schema = {type: 'object', properties: {myInt: {type: 'integer'}}}
+      // accepts missing properties
+      expect(s.validate({}, schema)).to.be.true
+      expect(s.validate({myInt: 1}, schema)).to.be.true
+      // accepts additional properties
+      expect(s.validate({myInt: 1, another: 2}, schema)).to.be.true
+      expect(s.validate({myInt: '1'}, schema)).to.be.false
+      expect(s.validate([], schema)).to.be.false
+      expect(s.validate('str', schema)).to.be.false
+      expect(s.validate(false, schema)).to.be.false
+      expect(s.validate(1, schema)).to.be.false
+      expect(s.validate(/.*/, schema)).to.be.false
+    })
+    it('validateSchema', function () {
+      expect(s.validateSchema({type: 'object', properties: {myInt: {type: 'integer'}}})).to.be.true
+      expect(s.validateSchema({type: 'object'})).to.be.false
+    })
+    it('normalize', function () {
+      // sets defaults
+      expect(s.normalize({}, {type: 'object', properties: {test: {type: 'integer', default: 3}}})).to.eql({test: 3})
+      // ignores empties
+      expect(s.normalize({}, {type: 'object', properties: {test: {type: 'integer'}}})).to.eql({})
+      // removes extras
+      expect(s.normalize({not: 3}, {type: 'object', properties: {test: {type: 'integer'}}})).to.eql({})
+    })
+  })
+})
